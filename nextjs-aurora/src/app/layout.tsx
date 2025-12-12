cdb14acbb7ac0db4ec40712a9c6f100a647b2c1a
Griffin Peterson <griff.pete31@gmail.com>
2025-12-12 00:27:53 -0700

    Fixed sanity image import, for vercel

--- nextjs-aurora/src/app/layout.tsx
+++ nextjs-aurora/src/app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { client } from "@/sanity/client";
-import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import imageUrlBuilder, { type SanityImageSource } from "@sanity/image-url";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
